c43e025c92955ebdf430593d51f2c1b05042b6a5
DuongKien <ddk0705@gmail.com>
2021-11-06 07:48:16 +0700

    Logic to handle drag event

--- js/coral.js
+++ js/coral.js
@@ -9,6 +9,8 @@ export default class Coral {
     this.sliderInterval = null;
     this.viewportInterval = null;
     this.leftOffset = 0;
+    this.drag = false;
+    this.startPoint = null;
     this.setup(obj);
     this.init();
   }
@@ -16,7 +18,7 @@ export default class Coral {
   setup(obj) {
     if (obj) {
       this.stepWidth = obj.stepWidth ? obj.stepWidth : 500;
-      this.speed = obj.speed ? obj.speed : 2;
+      this.speed = obj.speed ? obj.speed : 1;
       this.autoplaySpeed = obj.autoplaySpeed ? obj.autoplaySpeed : 0;
     }
   }
@@ -24,6 +26,12 @@ export default class Coral {
   init() {
     if ($('.coral-slider')) {
       this.sliderElement = $('.coral-slider').children().clone();
+      this.handleMouseEnter();
+      this.handleMouseLeave();
+      this.handleTouchStart();
+      this.handleTouchMove();
+      this.handleTouchEnd();
+      this.handleTouchCancel();
       this.getSliderWidth();
       this.trackingViewport();
       this.runningSlider();
@@ -39,21 +47,83 @@ export default class Coral {
     }
   }
 
+  handleMouseEnter() {
+    $('.coral-slider').on('mouseenter', () => {
+      this.destroyRunning();
+    });
+  }
+
+  handleMouseLeave() {
+    $('.coral-slider').on('mouseleave', () => {
+      this.runningSlider();
+    });
+  }
+
+  handleTouchStart() {
+    $('.coral-slider').on('touchstart mousedown', (event) => {
+      event.preventDefault();
+      this.drag = true;
+      this.startPoint = event.originalEvent.screenX;
+    });
+  }
+
+  handleTouchMove() {
+    $('.coral-slider').on('touchmove mousemove', (event) => {
+      event.preventDefault();
+    });
+  }
+
+  animation(offset, speed) {
+    $('.coral-slider').css('transform', `translate3d(${offset}px, 0px, 0px)`);
+    $('.coral-slider').css('transition', `transform ${speed}s`);
+  }
+
+  handleTouchEnd() {
+    $('.coral-slider').on('touchend mouseup', (event) => {
+      event.preventDefault();
+      const element = $('.coral-slider').offset();
+      const start = this.startPoint;
+      const end = event.originalEvent.screenX;
+      const leftOffset = element.left - start + end;
+      this.animation(leftOffset, this.speed);
+      this.startPoint = null;
+      this.drag = false;
+    });
+  }
+
+  handleTouchCancel() {
+    $('.coral-slider').on('touchcancel mouseleave', (event) => {
+      event.preventDefault();
+    });
+  }
+
   runningSlider() {
     this.sliderInterval = setInterval(() => {
       this.leftOffset = Math.ceil($('.coral-slider').find('.coral-item').first().offset().left);
       $('.coral-slider').css('transform', `translate3d(${this.leftOffset - this.stepWidth}px, 0px, 0px)`);
       $('.coral-slider').css('transition', `transform ${this.speed}s`);
+      this.animation(this.leftOffset - this.stepWidth, this.speed);
     }, this.autoplaySpeed);
   }
 
   trackingViewport() {
     this.viewportInterval = setInterval(() => {
       const windowWidth = $(window).width();
-      if (((this.leftOffset * -1) + windowWidth) >= (this.sliderWidth * this.sliderElementCount - 100)) {
-        this.sliderElement.appendTo('.coral-slider');
-        this.sliderElementCount += 1;
+      if (((this.leftOffset * -1) + windowWidth) >= (this.sliderWidth * this.sliderElementCount - 500)) {
+        this.appendToSlider();
       }
     }, 1000);
+  }
+
+  appendToSlider() {
+    this.sliderElement = this.sliderElement.clone();
+    this.sliderElement.appendTo('.coral-slider');
+    this.sliderElementCount += 1;
+  }
+
+  destroyRunning() {
+    if (this.sliderInterval) {
+      clearInterval(this.sliderInterval);
+    }
   }
 }
